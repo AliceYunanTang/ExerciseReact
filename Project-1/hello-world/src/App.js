e82db8518a060f40eeb697c247675b5129518664
AliceYunanTang <tangyunan@hotmail.com>
2017-08-16 23:37:34 +1000

    completed hello-warld

--- Project-1/hello-world/src/App.js
+++ Project-1/hello-world/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './index.css';
+import Header from './Header';
+import Intro from './Intro';
 
 class App extends Component {
   render() {
@@ -9,31 +11,6 @@ class App extends Component {
       <div className="App">
         <Header />
         <Intro />  
-      </div>
-    );
-  }
-}
-
-class Header extends Component {
-  render(){
-    return(
-      <div>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-      </div>
-    );
-  }
-}
-
-class Intro extends Component {
-  render(){
-    return(
-      <div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
